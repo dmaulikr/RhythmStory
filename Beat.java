ebcdcc3cb6f6ca522756c946edbb318b86b3f818
HentaiAddict <yuan.joe12@gmail.com>
2017-05-06 18:06:58 -0700

    Add files via upload

---
+++ Beat.java
@@ -0,0 +1,52 @@
+public class Beat
+{
+    int x;
+    int y;
+    int color; //1 = red 2 = blue 3 = both
+    boolean hit = false;
+    public Beat(int c)
+    {
+        x = 1024;
+        y = 605;
+        color = c;
+    }
+    public int getX()
+    {
+        return x;
+    }
+    public int getY()
+    {
+        return y;
+    }
+    public int getColor()
+    {
+        return color;
+    }
+    public void move()
+    {
+        x = x - 5;
+    }
+    public void hit()
+    {
+        hit = true;
+    }
+    public boolean getHit()
+    {
+        return hit;
+    }
+    public String getFile()
+    {
+        if (color == 1)
+        {
+            return "assets\\beatRed.png";
+        }
+        else if (color == 2)
+        {
+            return "assets\\beatBlue.png";
+        }
+        else
+        {
+            return "assets\\beatBoth.png";
+        }
+    }
+}
